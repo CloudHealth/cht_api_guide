066b3b495011e9f57c90f168d35cf5bc338b2a1f
Joe Kinsella <joe.kinsella@gmail.com>
2014-09-08 18:52:07 -0400

    Adding Python example

---
+++ examples/python/export_csv.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import urlparse
+import urllib2
+import json
+
+API_ENDPOINT = "https://apps.cloudhealthtech.com/olap_reports/"
+API_KEY = "<your api key>"
+
+# Returns json for requested report.
+def get_report(report, api_key):
+  uri = urlparse.urljoin(API_ENDPOINT, report)
+  uri += "?api_key=%s" % API_KEY
+  response = urllib2.urlopen(uri)
+  page = response.read()
+  return json.loads(page)
+
+# Fetch the json for the report
+try:
+  data = get_report("cost/history", API_KEY)
+
+  # Get list of dimension names
+  dimensions = [dim.keys()[0] for dim in data["dimensions"]]
+
+  # Output a CSV for this report
+  print "Month,%s" % ",".join(data['dimensions'][1][dimensions[1]])
+  index = 0
+  for month in data["dimensions"][0][dimensions[0]]:
+    row = data['data'][index]
+    if row == None:
+      continue
+
+    # We have only selected 1 measure so just take first element of every array
+    row_as_array = [str(item[0]) for item in row]
+    print ",".join(row_as_array)
+    print "%s,%s" % (month, ",".join(row_as_array))
+    index+=1
+
+except urllib2.HTTPError as e:
+  print e.code
+  print e.read()
